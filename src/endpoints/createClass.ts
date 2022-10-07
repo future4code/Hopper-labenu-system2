bdb286092f5a6c1ad4cdde2a2aaf881723af471e
Gabriel Cândido <gabrielcandido10@hotmail.com>
2022-10-06 22:11:45 -0300

    BaseDatabase, Models, TurmaDatabase e endpoints criados

--- src/endpoints/createClass.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response} from "express";
-import connection from "../dataBase/connection";
-
-export const createClass = async (req: Request, res: Response):Promise<void> => {
-  try {
-  } catch (error: any) {
-    res.send(error.message || error.sqlMessage)
-  }
-}
