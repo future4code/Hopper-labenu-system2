c6446b0791a936ed43807943e0834e1d0835d58b
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-10-08 00:38:36 -0300

    Endpoints ainda incompletos

---
+++ src/models/Docente.ts
@@ -0,0 +1,60 @@
+export class Docente {
+    constructor(
+      private id: string,
+      private nome: string,
+      private email: string,
+      private data_nasc: string,
+      private turma_id: string
+    ){
+      this.id = id
+      this.nome = nome
+      this.email = email
+      this.data_nasc = data_nasc
+      this.turma_id = turma_id
+    }
+
+    //GETTERS
+  
+    public getId(){
+      return this.id
+    }
+  
+    public getNome(){
+      return this.nome
+    }
+  
+    public getEmail(){
+      return this.email
+    }
+
+    public getDataNasc(){
+        return this.data_nasc
+      }
+
+    public getIdTurma(){
+        return this.turma_id
+    }
+
+
+    //SETTERS
+  
+    public setId(newId: string){
+      this.id = newId
+    }
+  
+    public setNome(newNome: string){
+      this.nome = newNome
+    }
+  
+    public setEmail(newEmail: string){
+      this.email = newEmail
+    }
+
+    public setDataNasc(newDate: string){
+        this.data_nasc = newDate
+      }
+
+    public setIdTurma(newGang: string){
+        this.turma_id = newGang
+    }
+  }
